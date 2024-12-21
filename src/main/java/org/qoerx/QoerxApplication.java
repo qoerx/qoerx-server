1f923e4711e923f78493186be83e94bbc8c777a5
王硕 <18615498639@163.com>
2024-12-21 09:12:30 +0800

    build: 初始化项目
    1.初始化项目
    2.引入druid连接池
    3.添加启动类

---
+++ src/main/java/org/qoerx/QoerxApplication.java
@@ -0,0 +1,15 @@
+package org.qoerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: wangshuo
+ * Date: 2024/12/20 下午9:49
+ */
+@SpringBootApplication
+public class QoerxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QoerxApplication.class, args);
+    }
+}
